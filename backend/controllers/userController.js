4179662b8dfc57e7510f74bdae5a5bebb7d0e6bb
Ashish2271 <ashispandey138c@gmail.com>
2023-10-24 19:37:56 +0530

    Notify

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -85,7 +85,7 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
   try {
     await sendEmail({
       email: user.email,
-      subject: `Ecommerce Password Recovery`,
+      subject: `Notify Password Recovery`,
       message,
     });
 
