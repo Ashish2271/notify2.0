bd458dcb98af8ed686c441bdb63a500a0eeb19ea
Ashish2271 <ashispandey138c@gmail.com>
2023-10-24 19:31:51 +0530

    fix reset password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,7 +78,7 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false });
 
-  const resetPasswordUrl = `${req.protocol}://${req.get("host")}/password/reset/${resetToken}`;
+  const resetPasswordUrl = `http://localhost:5173/reset/${resetToken}`;
 
   const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
 
@@ -135,7 +135,7 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
 
   await user.save();
 
-  sendToken(user, 200, res);
+  sendToken(user, "welcome", 200, res);
 });
 
 
